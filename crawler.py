f1941dc0c5e201552847dc358241a1c4fef9edd3
Jonathan Dickinson <jon.dickinson17@gmail.com>
2022-07-20 11:33:08 -0700

    Add function that retrieves url for the next page

--- crawler.py
+++ crawler.py
@@ -5,34 +5,31 @@ from bs4 import BeautifulSoup
 import requests
 
 # Grab website URL
-URL = 'https://www.amazon.com/s?k=coffee&crid=A34N1M202SCT&sprefix=coffee%2Caps%2C231&ref=nb_sb_noss_1'
+URL = 'https://www.amazon.com/s?k=coffee'
 headers={
     'Host': 'www.amazon.com',
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
 
-# get the page content and parse
+# get the page content
 def getSoup(url, headers):
     r = requests.get(url=URL, headers=headers)
     soup = BeautifulSoup(r.content, 'html.parser')
     return soup
 
-# get title of all the coffee on the page
-def getTitle(soup):
-    #names = []
-    coffeeName= soup.find('span', class_="a-size-base-plus").get_text()
-    return coffeeName
+def getNextPage(soup):
+    for link in soup.find_all('a', class_="s-pagination-next"):
+        newURL = 'https://www.amazon.com' + link.get('href')
+        return newURL
 
-# find corresponding price for all coffee
-def findPrice(soup):
+def getData(soup):
+    # get title & price of all the coffee on the page
+    coffeeName= soup.find('span', class_="a-size-base-plus").get_text()    
     coffeePrice = soup.find('span', class_="a-price-whole").get_text()
     return coffeePrice
 
-
 def main():
-    soup = getSoup(URL, headers)
-    coffeeName = getTitle(soup)
-    coffeePrice = findPrice(soup)
-    print(coffeeName, coffeePrice)
+    soup= getSoup(URL, headers)
+    getNextPage(soup)
 
 if __name__ =='__main__':
     main()
